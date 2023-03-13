076ba2fa8ef42188dea94cdf8d8ec061e104818a
TheodoreBillotte <theodore.billotte@epitech.eu>
2023-03-13 21:40:32 +0100

    GraphiC [fix]: sliders implementation and memory leaks

--- src/sliders_functions.c
+++ src/sliders_functions.c
@@ -7,6 +7,21 @@
 
 #include <malloc.h>
 #include "sliders.h"
+#include "textures.h"
+
+void slider_set_values(graphic_t *graphic, slider_t *slider,
+                        slider_constructor_t constructor)
+{
+    sfSprite_setTexture(slider->bar, get_texture(graphic, constructor.bar)
+                ->texture, sfTrue);
+    sfSprite_setTexture(slider->scroller, get_texture(graphic,
+                constructor.scroller)->texture, sfTrue);
+    sfSprite_setPosition(slider->bar, constructor.pos);
+    sfSprite_setPosition(slider->scroller, constructor.pos);
+    sfFloatRect bar_rect = sfSprite_getGlobalBounds(slider->bar);
+    sfSprite_setScale(slider->bar, (sfVector2f) {constructor.size.x /
+                bar_rect.width,constructor.size.y / bar_rect.height});
+}
 
 void destroy_slider_list(list_t *list)
 {
