1518187e1b0c708d7e4f6fe514f26cda054732db
TheodoreBillotte <theodore.billotte@epitech.eu>
2023-03-14 16:57:16 +0100

    GraphiC [add]: can create texture_t type using sfTexture

--- src/textures.c
+++ src/textures.c
@@ -8,29 +8,36 @@
 #include <malloc.h>
 #include "graphic.h"
 
-texture_t * build_texture(graphic_t * graphic, char * path)
+texture_t *build_texture(graphic_t *graphic, char *path)
 {
-    texture_t * texture;
-    sfTexture * image = sfTexture_createFromFile(path, NULL);
+    texture_t *texture;
+    sfTexture *image = sfTexture_createFromFile(path, NULL);
 
     if (image == NULL)
         return NULL;
-
     texture = malloc(sizeof(texture_t));
-
     texture->texture = image;
     texture->id = list_size(graphic->textures);
     list_append(graphic->textures, texture);
-
     return texture;
 }
 
-texture_t * get_texture(graphic_t * graphic, int id)
+texture_t *build_from_texture(graphic_t *graphic, sfTexture *texture)
+{
+    texture_t *new_texture = malloc(sizeof(texture_t));
+
+    new_texture->texture = texture;
+    new_texture->id = list_size(graphic->textures);
+    list_append(graphic->textures, new_texture);
+    return new_texture;
+}
+
+texture_t *get_texture(graphic_t *graphic, int id)
 {
     return list_get(graphic->textures, id);
 }
 
-void destroy_texture(texture_t * texture)
+void destroy_texture(texture_t *texture)
 {
     sfTexture_destroy(texture->texture);
     free(texture);
